32db4dbd96ba49662a67e952655eac73ad6ada48
PRAKASH-VD <prakashdjp4@gmail.com>
2025-08-11 20:42:09 +0530

    link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://real-estate-backend-m26k.onrender.com/api",
 });
 
 export default api;
